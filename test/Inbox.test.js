e3a14926f6626210d359ad81927a2bcf6e0e13b3
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-09-29 06:08:18 +0530

    Fetching Accounts using async await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,12 +3,11 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-beforeEach( ()=> {
+let accounts;
+
+beforeEach(async ()=> {
   // Get a list of all accounts.
-web3.eth.getAccounts()
-  .then(fetchedAccounts => {
-    console.log(fetchedAccounts);
-  });
+accounts = await web3.eth.getAccounts();
   //Use one of those accounts to deploy the contract.
 
   //
@@ -16,6 +15,6 @@ web3.eth.getAccounts()
 
 describe('Inbox', ()=> {
   it('deploys a contract', ()=>{
-    
+    console.log(accounts);
   });
 });
